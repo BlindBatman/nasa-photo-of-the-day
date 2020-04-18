26da8eb99bd61781fc38f6d6d3198766d774c209
blindbatman <emlundaj@gmail.com>
2020-04-17 21:21:13 -0600

    day two made pretty

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import "./App.css";
+import "./App.css"
+import Pic from "./components/photo"
+import Head from "./components/header"
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Head/>
+      <Pic/>
     </div>
   );
 }
